e9a9d0759e80bea1dccbc778d59e1e6460199b67
Craig Edwards <craigedwards@brainbox.cc>
2021-05-01 08:08:36 +0100

    fix needless second decl

--- modules/trivia/trivia.cpp
+++ modules/trivia/trivia.cpp
@@ -375,7 +375,7 @@ guild_settings_t TriviaModule::GetGuildSettings(dpp::snowflake guild_id)
 std::string TriviaModule::GetVersion()
 {
 	/* NOTE: This version string below is modified by a pre-commit hook on the git repository */
-	std::string version = "$ModVer 86$";
+	std::string version = "$ModVer 87$";
 	return "3.0." + version.substr(8,version.length() - 9);
 }
 
@@ -516,7 +516,6 @@ void TriviaModule::CheckForQueuedStarts()
 		if (g) {
 
 			uint64_t channel_id = from_string<uint64_t>((*r)["channel_id"], std::dec);
-			uint64_t guild_id = from_string<uint64_t>((*r)["guild_id"], std::dec);
 			uint64_t user_id = from_string<uint64_t>((*r)["user_id"], std::dec);
 			uint32_t questions = from_string<uint32_t>((*r)["questions"], std::dec);
 			uint32_t quickfire = from_string<uint32_t>((*r)["quickfire"], std::dec);
