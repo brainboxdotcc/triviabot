be899cb99055300b0c691ec41960447003789f1e
brain <craigedwards@brainbox.cc>
2021-10-05 09:22:12 +0100

    stop pre-commit hook trampling on all files on git commit

--- hooks/pre-commit.php
+++ hooks/pre-commit.php
@@ -9,14 +9,18 @@ foreach ($file_list as $file) {
 	if (!empty($file) && preg_match('/^modules\/.+?.cpp$/', $file)) {
 		$source = file_get_contents($file);
 		$version = 1;
+		$found = false;
 		$source = preg_replace_callback('/\$ModVer (\d+)\$/', function(array $matches) {
 			$current_version = intval($matches[1]) + 1;
 			$version = $current_version;
+			$found = true;
 			return "\$ModVer $current_version$";
-		}, $source);	
-		$source = preg_replace('/\$ModVer\$/m', '$ModVer 1$', $source);
-		print "$file => version $version\n";
-		file_put_contents($file, $source);
-		system("/usr/bin/git add $file");
+		}, $source);
+		if ($found) {
+			$source = preg_replace('/\$ModVer\$/m', '$ModVer 1$', $source);
+			print "$file => version $version\n";
+			file_put_contents($file, $source);
+			system("/usr/bin/git add $file");
+		}
 	}
 }
