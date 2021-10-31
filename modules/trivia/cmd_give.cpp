86f0648eeed2708e85decb3e17311c6f7e7a76fd
brain <craigedwards@brainbox.cc>
2021-10-31 13:23:01 +0000

    fix /give not allowing to give if someone has a balance of zero

--- modules/trivia/cmd_give.cpp
+++ modules/trivia/cmd_give.cpp
@@ -54,7 +54,7 @@ void command_give_t::call(const in_cmd &cmd, std::stringstream &tokens, guild_se
 		user_id = cmd.author_id;
 	}
 
-	db::resultset coins = db::query("SELECT * FROM coins WHERE user_id = '?'", {user_id});
+	db::resultset coins = db::query("SELECT * FROM coins WHERE user_id = '?'", {cmd.author_id});
 	if (coins.size()) {
         	balance = from_string<uint64_t>(coins[0]["balance"], std::dec);
 	}
