d4709a8295ccccc42a4a89864d5580f71a7f80c2
Craig Edwards <craigedwards@brainbox.cc>
2024-10-11 18:17:25 +0000

    fix: entitlements now work correctly again

--- update-entitlements.php
+++ update-entitlements.php
@@ -43,11 +43,12 @@ do {
 
     $now = new Carbon();
     foreach ($subscriptions as $subscription) {
-        if (in_array($subscription->type, [6, 8])) {
+        if (in_array($subscription->type, [1])) {
            $start = new Carbon($subscription->starts_at ?? null);
-	    $end = new Carbon($subscription->ends_at ?? null);
+	   $end = !empty($subscription->ends_at) ? new Carbon($subscription->ends_at) : (new Carbon())->addMonths(1);
+	   $subscription->subscription_id = $subscription->user_id . '.' . $subscription->guild_id;;
 	    echo "SUBSCRIPTION: $subscription->subscription_id USER: $subscription->user_id GUILD: $subscription->guild_id ACTIVE: " . ($now->between($start, $end) ? 1 : 0) . " ";
-	    echo "START: " .(is_object($start) ? $start->format('Y-m-d H:i:s') : 'N/A') . " END: " . (is_object($end) ? $end->format('Y-m-d H:i:s') : 'N/A') . " TYPE: " . $subscription->type . "\n\n";
+	    echo "START: " .(is_object($start) ? $start->format('Y-m-d H:i:s') : 'N/A') . " END: '$subscription->ends_at' " . (is_object($end) ? $end->format('Y-m-d H:i:s') : 'N/A') . " TYPE: " . $subscription->type . "\n\n";
 	    if (!empty($subscription->subscription_id)) {
                 mysqli_query($conn, "INSERT INTO premium_credits (user_id, subscription_id, guild_id, active, since, plan_id, payment_failed) VALUES($subscription->user_id, $subscription->subscription_id, $subscription->guild_id, 1, now(), 'triviabot-premium-monthly', 0) ON DUPLICATE KEY UPDATE active = " . ($now->between($start, $end) ? 1 : 0));
                 mysqli_query($conn, "UPDATE bot_guild_settings SET premium = " . ($now->between($start, $end) ? 1 : 0) . " WHERE snowflake_id = " . $subscription->guild_id);
